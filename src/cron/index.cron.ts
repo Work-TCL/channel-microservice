07271e4812a1b11027cf26996701f8909c0481ca
Akshay <akshay10sangani@gmail.com>
2025-07-19 23:13:28 +0530

    cron added for blocked amount

--- src/cron/index.cron.ts
+++ src/cron/index.cron.ts
@@ -1,23 +1,24 @@
 import cron from "node-cron";
 import { getMidnightYTData } from "../controller/creator/youtube/youtube.controller";
 import { getMidnightInstagramData, refreshInstagramToken } from "../controller/creator/instagram/auth.controller";
+import { releaseBlockedAmounts } from "../controller/shopify/webhook/shopifyWebhook.controller";
 
 //Run at 1 Am every day
-cron.schedule("0 1 * * *", async() => {
-    await getMidnightYTData();
-}, {
-    scheduled: true,
-    timezone: "Asia/Kolkata" // Indian Standard Time (IST)
-});
+// cron.schedule("0 1 * * *", async() => {
+//     await getMidnightYTData();
+// }, {
+//     scheduled: true,
+//     timezone: "Asia/Kolkata" // Indian Standard Time (IST)
+// });
 
 //Run at 2 Am every day
-cron.schedule("0 2 * * *", async () => {
-    console.log("Running instagram video data fetch 2 AM IST...");
-    await getMidnightInstagramData()
-}, {
-    scheduled: true,
-    timezone: "Asia/Kolkata"
-});
+// cron.schedule("0 2 * * *", async () => {
+//     console.log("Running instagram video data fetch 2 AM IST...");
+//     await getMidnightInstagramData()
+// }, {
+//     scheduled: true,
+//     timezone: "Asia/Kolkata"
+// });
 
 //Run at 3 Am every day
 cron.schedule("0 3 * * *", () => {
@@ -28,6 +29,13 @@ cron.schedule("0 3 * * *", () => {
     timezone: "Asia/Kolkata"
   });
   
+ // Run at 1 Am every day
+cron.schedule("0 1 * * *", async() => {
+    await releaseBlockedAmounts();
+}, {
+    scheduled: true,
+    timezone: "Asia/Kolkata" // Indian Standard Time (IST)
+});
 
 // cron.schedule("* * * * *", async () => {
 //     console.log("Running task every minute...");
